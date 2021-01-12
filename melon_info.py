b3c8526d9f1a3a6e2cd005863874508c370e6e95
keiva <jen@silverwire.ninja>
2021-01-12 10:02:39 -0700

    Completed melons.py; added print statements to melon_info.py

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,25 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import (melon_names, melon_seedlessness, melon_prices, melon_flesh_color,
+                    melon_weight, melon_rind_color)
 
 
 def print_melon(name, seedless, price, flesh, weight, rind):
     """Print each melon with corresponding attribute information."""
 
-    have_or_have_not = 'have'
+    have_or_have_not = 'Not Seedless'
     if seedless:
-        have_or_have_not = 'do not have'
+        have_or_have_not = 'Seedless'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    print(f'{name}s are {have_or_have_not} and cost ${price:.2f}')
 
 
 for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+    print_melon(melon_names[i],
+                melon_seedlessness[i],
+                melon_prices[i],
+                melon_flesh_color[i],
+                melon_weight[i],
+                melon_rind_color[i]
+                )
--- melons.py
+++ melons.py
@@ -21,3 +21,27 @@ melon_seedlessness = {
     4: False,
     5: False,
 }
+
+melon_flesh_color = {
+    1: 'Green',
+    2: 'Yellow, White',
+    3: 'Orange',
+    4: 'White',
+    5: 'Orange',
+}
+
+melon_weight = {
+    1: 3.0,
+    2: 3.1,
+    3: 4.0,
+    4: 5.2,
+    5: 5.0,
+}
+
+melon_rind_color = {
+    1: 'Ivory Green',
+    2: 'Green, Orange Spots when ripe',
+    3: 'Green, Orange Spots when ripe',
+    4: 'Yellow',
+    5: 'Netted Skin',
+}
